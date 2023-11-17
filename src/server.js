b6e0210e2d436c4452618fdfc507484155771bcc
Vitor Silva <vitorsilva10413@gmail.com>
2023-11-17 20:24:46 +0000

    Remove process.stdin

--- src/server.js
+++ src/server.js
@@ -5,12 +5,3 @@ app.server = app.listen(app.address.port, app.address.host, () => {
     `Server is running at http://${app.address.host}:${app.address.port}`,
   );
 });
-
-process.stdin.on('data', (data) => {
-  const dataStr = data.toString().replace(/\n/, '');
-  if (dataStr === 'url') {
-    console.info(`http://${app.address.host}:${app.address.port}`);
-  } else {
-    process.exit();
-  }
-});
